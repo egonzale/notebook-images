bf59262689fc377f2784e172db0c7e29f191c2d2
egonzale <eglatorre+t1@gmail.com>
2019-02-05 07:44:51 +0200

    init file to test notebooks/rahti

---
+++ builds/tests/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Change to default working directory
+cd /home/jovyan/work/
+
+# git reflog requires a name and email if user is not in passwd
+# even if you're only cloning
+export GIT_COMMITTER_NAME=anonymous
+export GIT_COMMITTER_EMAIL=anon@localhost
+
+# Clone notebooks to ~/work/notebooks
+git clone https://github.com/thiippal/ENG-3072/
+
+# Go to notebooks folder
+# cd /home/jovyan/work/ENG-3072/
+
+jupyter notebook --notebook-dir='/home/jovyan/work/ENG-3072/'
